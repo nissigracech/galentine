8d7a80cb40fda53cfdbbd9995092977d5d6a9870
nissigracech <nissigracech@gmail.com>
2025-02-13 13:36:06 +0530

    Updated requirements

--- galentine.py
+++ galentine.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 
 # Set page config
 st.set_page_config(page_title="Galentine's Day 💖", page_icon="💌", layout="centered")
@@ -14,9 +15,16 @@ page_bg_color = """
     color: white !important;
     border-radius: 15px;
     font-size: 20px;
+    transition: all 0.3s ease-in-out;
+}
+
+.move {
+    position: absolute;
+    left: {}px;
+    top: {}px;
 }
 </style>
-"""
+""".format(random.randint(50, 300), random.randint(50, 300))
 st.markdown(page_bg_color, unsafe_allow_html=True)
 
 # Title & message
@@ -25,43 +33,43 @@ st.header("On this Galentine's Day, I want to ask you something")
 st.subheader("Will you be my Galentine 🥰💌?")
 st.image("gal.jpg", width=300)
 
-# Session state initialization
+# Session state to handle button clicks
 if 'declined' not in st.session_state:
     st.session_state.declined = False
 if 'accepted' not in st.session_state:
     st.session_state.accepted = False
 
-if not st.session_state.accepted and not st.session_state.declined:
-    # First set of buttons (shown initially)
-    col1, col2 = st.columns([1, 1])  # col1 for buttons, col2 empty
-    with col1:
-        sub_col1, sub_col2 = st.columns(2)
+col1, col2 = st.columns([1, 2])  # col2 is empty
+with col1:
+    sub_col1, sub_col2 = st.columns(2)
+
+    if not st.session_state.accepted and not st.session_state.declined:
         with sub_col1:
-            if st.button("Yes🥹🥹, I will", key="yes"):
+            if st.button("Yes🥹🥹, I will"):
                 st.session_state.accepted = True
-                st.session_state.declined = False
                 st.balloons()
+                st.success("Yay! You're my Galentine forever! 💕✨")
+
         with sub_col2:
-            if st.button("No😒, I can't", key="no"):
+            move_style = ""
+            if st.button("No😒, I can't"):
                 st.session_state.declined = True
-                st.session_state.accepted = False
                 st.rerun()
+            else:
+                move_style = "move"
+            
+            st.markdown(
+                f"<style>.stButton>button {{ position: absolute; left: {random.randint(50, 300)}px; top: {random.randint(50, 300)}px; }}</style>",
+                unsafe_allow_html=True
+            )
 
-if st.session_state.accepted:
-    st.success("Yay! You're my Galentine forever! 💕✨")
-
-if st.session_state.declined and not st.session_state.accepted:
-    # Show "You can't escape!" message first
+if st.session_state.declined:
     st.subheader("You can't escape! Think again! 😜💖")
-
-    # Second set of buttons (after clicking "No")
-    col1, col2 = st.columns([1, 1])  # Same layout
-    with col1:
-        sub_col1, sub_col2 = st.columns(2)
-        with sub_col1:
-            if st.button("😒 No 😒", key="no_again"):
-                st.rerun()
-        with sub_col2:
-            if st.button("🤔 Think Again 🤔", key="think_again"):
-                st.session_state.declined = False
-                st.rerun()
+    sub_col1, sub_col2 = st.columns(2)
+    with sub_col1:
+        if st.button("😒 No 😒"):
+            st.rerun()
+    with sub_col2:
+        if st.button("🤔 Think Again 🤔"):
+            st.session_state.declined = False
+            st.rerun()
